dd327573353973b80957f326faa7c77f1649f667
Alejandro Sanchez <alejandro-17091@hotmail.com>
2025-07-08 03:46:58 -0500

    update

--- src/main/java/com/example/crud_webflux_api/CrudWebfluxApiApplication.java
+++ src/main/java/com/example/crud_webflux_api/CrudWebfluxApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
 
 //@EnableReactiveMongoRepositories(basePackages = "com.example.crud_webflux_api.repository")
 @SpringBootApplication
